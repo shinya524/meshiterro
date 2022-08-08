5df558319bf6a2ee3ba2d9f4ebc8441b576491ca
shinya524 <gheena1800@gmail.com>
2022-08-08 02:54:27 +0000

    [Finish]4-16

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,17 @@ class ApplicationController < ActionController::Base
     about_path
   end
 
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
+
   protected
 
   def configure_permitted_parameters
